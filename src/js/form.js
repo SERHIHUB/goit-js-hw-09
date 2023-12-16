0f523da0d29078ff9349fe4c12b7a135ead971c1
SERHIHUB <chokorik@gmail.com>
2023-12-16 18:39:44 +0200

    fixed

--- src/js/form.js
+++ src/js/form.js
@@ -3,31 +3,26 @@ const localStorageKey = 'feedback-form-state';
 
 try {
   const initialForm = JSON.parse(localStorage.getItem(localStorageKey));
-
-  Array.from(form.elements).forEach(element => {
-    const carrentValue = initialForm[element.name];
-    if (carrentValue) {
-      element.value = carrentValue;
-    }
-  });
 } catch (error) {
   console.error('ERROR FORM');
 }
 
 form.addEventListener('input', event => {
-  const formObj = {};
   const formData = new FormData(form);
+  const formObj = {};
 
   formData.forEach((value, key) => {
     formObj[key] = value;
   });
 
   localStorage.setItem(localStorageKey, JSON.stringify(formObj));
-  console.log(formObj);
 });
 
 form.addEventListener('submit', event => {
   event.preventDefault();
+  const obj = JSON.parse(localStorage.getItem(localStorageKey));
+  console.log(obj);
   localStorage.removeItem(localStorageKey);
+
   form.reset();
 });
